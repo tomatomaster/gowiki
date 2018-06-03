b40938fb8fa037ec1ccf7062ad8d5c4913a64871
tomatomaster <iiitomatomasteriii@gmail.com>
2018-06-03 23:19:49 +0900

    チャット機能追加

--- wiki.go
+++ wiki.go
@@ -7,13 +7,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
 const pageStoredPath = "data/"
 
-var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html", "templates/chat.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view|chat)/([a-zA-Z0-9]+)$")
 
 // Page is wikipage
 type Page struct {
@@ -21,8 +24,53 @@ type Page struct {
 	Body  []byte
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi, there %s", r.URL.Path[1:])
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(pageStoredPath+filename, p.Body, 0600)
+}
+
+// ChatLog is chat log
+type ChatLog struct {
+	Name    string
+	Comment string
+	Date    string
+}
+
+// ViewLog shows all Chatlog
+type ViewLog struct {
+	Logs []string
+}
+
+func (c ChatLog) saveLog() error {
+	f, err := os.OpenFile(pageStoredPath+"chatLog", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	logFormat := fmt.Sprintf("%s【%s】%s\n", c.Date, c.Name, c.Comment)
+	_, err = f.Write([]byte(logFormat))
+	return err
+}
+
+func (c ChatLog) readAllLog() *ViewLog {
+	logs, err := ioutil.ReadFile(pageStoredPath + "chatLog")
+	if err != nil {
+		log.Fatal(err)
+	}
+	viewLog := new(ViewLog)
+	for _, line := range strings.Split(string(logs), "\n") {
+		viewLog.Logs = append(viewLog.Logs, line)
+	}
+	viewLog.Logs = viewLog.Logs[:len(viewLog.Logs)-1]
+
+	return viewLog
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	chatLog := ChatLog{Name: r.FormValue("name"), Comment: r.FormValue("chat"), Date: time.Now().Format(time.Stamp)}
+	chatLog.saveLog()
+	viewLog := chatLog.readAllLog()
+	renderChatTemplate(w, "chat", viewLog)
 }
 
 func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
@@ -85,9 +133,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(pageStoredPath+filename, p.Body, 0600)
+func renderChatTemplate(w http.ResponseWriter, tmpl string, d *ViewLog) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func loadPage(title string) (*Page, error) {
@@ -102,6 +152,7 @@ func loadPage(title string) (*Page, error) {
 func main() {
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/chat/", chatHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
