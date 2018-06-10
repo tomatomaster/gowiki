df85cde93f90a6074dddfbe388d8c47e81ab8ca2
tomatomaster <iiitomatomasteriii@gmail.com>
2018-06-10 13:00:13 +0900

    リアルタイムチャットのサンプルを写経

--- wiki.go
+++ wiki.go
@@ -8,7 +8,20 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/gorilla/websocket"
 )
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
+
+//Message is sent by Websocket
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
 var templates = template.Must(template.ParseFiles("templates/chat.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view|chat)/([a-zA-Z0-9]+)$")
@@ -97,9 +110,48 @@ func renderChatTemplate(w http.ResponseWriter, tmpl string, d *ViewLog) {
 	}
 }
 
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	clients[ws] = true
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
 func main() {
-	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/chat/", chatHandler)
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	//http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
+	//http.HandleFunc("/", rootHandler)
+	//http.HandleFunc("/chat/", chatHandler)
+	log.Println("http server started on: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
